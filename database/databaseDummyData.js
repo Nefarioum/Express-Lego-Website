d6e7ad8fe9682bbac300a260fa2e1ddac266a321
Nefarioum <nefarioum@outlook.com>
2022-01-21 04:42:26 +0000

    feat(db-rewrite-2): Re-wrote database handler to be allow for actual async behaviour and code clean-up

--- database/databaseDummyData.js
+++ database/databaseDummyData.js
@@ -1,21 +1,20 @@
 import DatabaseHandler from './databaseManager.js'
 
 export const initializeDBTable = async () => {
-    DatabaseHandler.DatabaseConnection.serialize(() => {
-        DatabaseHandler.query("CREATE TABLE lego_items (unique_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, image TEXT, price INTEGER, stock INTEGER, slug TEXT, description TEXT, schema TEXT);");
-        DatabaseHandler.query("CREATE UNIQUE INDEX idx_lego_items_id ON lego_items (unique_id);");
-    });
-
-    createDummyData()
+    await DatabaseHandler.multipleQueries([
+        ["CREATE TABLE lego_items (unique_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, image TEXT, price INTEGER, stock INTEGER, slug TEXT, description TEXT, schema TEXT);", []],
+        ["CREATE UNIQUE INDEX idx_lego_items_id ON lego_items (unique_id);", []],
+    ]);
+    await createDummyData()
 };
 
 export const createDummyData = async () => {
-    DatabaseHandler.DatabaseConnection.serialize(() => {
-        DatabaseHandler.query("INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Blue)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'blue-brick-1', 'A blue lego brick for all your lego needs!', 'brick'])
-        DatabaseHandler.query("INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Yellow)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'yellow-brick-1', 'A yellow lego brick for all your lego needs!', 'brick'])
-        DatabaseHandler.query("INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Green)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'green-brick-1', 'A green lego brick for all your lego needs!', 'brick'])
-        DatabaseHandler.query("INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Orange)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'orange-brick-1', 'A orange lego brick for all your lego needs!', 'brick'])
-        DatabaseHandler.query("INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Black)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'black-brick-1', 'A black lego brick for all your lego needs!', 'brick'])
-        DatabaseHandler.query("INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Purple)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'pruple-brick-1', 'A purple lego brick for all your lego needs!', 'brick'])
-    });
+    await DatabaseHandler.multipleQueries([
+        ["INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Blue)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'blue-brick-1', 'A blue lego brick for all your lego needs!', 'brick']],
+        ["INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Yellow)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'yellow-brick-1', 'A yellow lego brick for all your lego needs!', 'brick']],
+        ["INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Green)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'green-brick-1', 'A green lego brick for all your lego needs!', 'brick']],
+        ["INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Orange)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'orange-brick-1', 'A orange lego brick for all your lego needs!', 'brick']],
+        ["INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Black)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'black-brick-1', 'A black lego brick for all your lego needs!', 'brick']],
+        ["INSERT INTO `lego_items` (name, image, price, stock, slug, description, schema) VALUES (?, ?, ?, ?, ?, ?, ?);", ['Lego Piece (Purple)', 'https://www.kindpng.com/picc/m/390-3908027_blue-lego-brick-transparent-hd-png-download.png', 5, 100, 'purple-brick-1', 'A purple lego brick for all your lego needs!', 'brick']],
+    ])
 };
--- database/databaseManager.js
+++ database/databaseManager.js
@@ -3,50 +3,87 @@ import DB from 'sqlite3';
 class DatabaseHandler {
     constructor() {
         this.DatabaseConnection = null
+        this.Database = null
 
         this.init();
     }
 
     async init() {
-        const Database = await new DB.Database('database/db/lego-store.db', async (err) => {
-            if (err) {
-              return console.error(err.message);
-            }
-    
-            Database.get("SELECT count(*) FROM sqlite_master WHERE type='table'AND name ='lego_items';", [], async (err, row) => {
-                if (err) return console.error(err.message);
-        
-                if (!row['count(*)']) {
-                    await Database.close();
-                    return this.initTable();
-                } 
+        const DatabaseRow = await this.query("SELECT count(*) FROM sqlite_master WHERE type='table'AND name ='lego_items';", []);
 
+        if (!DatabaseRow['count(*)']) {
+            return this.initTable();
+        };
+    }
+
+    async connection() {
+        return new Promise(resolve => {
+            const Database = new DB.Database('database/db/lego-store.db', (err) => {
+                if (err) {
+                    return console.error(err.message);
+                }
                 console.log('Connected to the in-memory SQlite database.');
 
                 this.DatabaseConnection = Database;
-              });
-          });;
-    }
 
-    async query(sql, parameters) {
-        await this.DatabaseConnection.get(sql, parameters, async (err, row) => {
-            if (err) return console.error(err.message);
-            console.log(sql);
-            return row;
+                resolve(Database);
+            });;
         });
     }
 
-    
-    async initTable(sql, parameters) {
-        const Database = await new DB.Database('database/db/lego-store.db', async (err) => {
-            console.log('Connected to the in-memory SQlite database.');
+    async close() {
+        await this.DatabaseConnection.close((err) => {
+            if (err) {
+              console.error(err.message);
+            }
 
-        });;
-
-        this.DatabaseConnection = Database;
-        await initializeDBTable();
+            console.log('Closed the database connection.');
+        });
     }
 
+
+    async query(sql, parameters) {
+        this.DatabaseConnection = await this.connection();
+
+        return new Promise(resolve => {
+            this.DatabaseConnection.get(sql, parameters, (err, row) => {
+                if (err) console.error(`${sql}- ${err.message}`);
+                this.close();
+
+                console.log('Single Query has executed successfully.')
+
+                resolve(row);
+            });
+        });
+    }
+
+    async multipleQueries(queriesParameter) {
+        this.DatabaseConnection = await this.connection();
+        let promises = [];
+
+        this.DatabaseConnection.serialize(() => {
+            queriesParameter.forEach((e) => {
+                promises.push(
+                    new Promise(resolve => {
+                        this.DatabaseConnection.get(e[0], e[1], (err, row) => {
+                            if (err) console.error(`${e[0]} - ${err.message}`);
+
+                            resolve(row);
+                        })
+                    })
+                );
+            })
+        });
+        
+        return Promise.all(promises).then(() => {
+            this.close()
+            console.log('Multiple Queries have all executed successfully.')
+        });
+    }
+    
+    async initTable() {       
+        await initializeDBTable();
+    }
 }
 
 const DataHandler = new DatabaseHandler();
