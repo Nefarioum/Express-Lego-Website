ad863d8ff90bade1ad5d0e37fd8619c02d505ef5
Nefarioum <nefarioum@outlook.com>
2022-04-02 01:02:19 +0100

    feat(404-page): Redirect the user to the 404 page if there is no result found for an item.

--- public/js/pages/item.js
+++ public/js/pages/item.js
@@ -9,17 +9,20 @@ const loadStoreItems = async () => {
     if (response.ok) {
         StoreItems = await response.json();
 
-        document.querySelector(`#name`).insertAdjacentHTML('afterbegin',
-        `<h1 class="text">${StoreItems[0].Name}</h1>
-         <p class="text">${StoreItems[0].Description}</p>
-
-         <div class="image"
-                style="background-image: url(${StoreItems[0].Image});">
-         </div>
-
-        `)
-    }
-
+        if (StoreItems.length > 0) {
+            document.querySelector(`#name`).insertAdjacentHTML('afterbegin',
+            `<h1 class="text">${StoreItems[0].Name}</h1>
+             <p class="text">${StoreItems[0].Description}</p>
+    
+             <div class="image"
+                    style="background-image: url(${StoreItems[0].Image});">
+             </div>
+    
+            `)
+        } else {
+            window.location.href = window.location.href.replace(`item#${getItemID()}`, '404-not-found');
+        }
+    } 
 }
 
 loadStoreItems()
